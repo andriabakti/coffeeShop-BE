922e9a83153f045f85f745bbd319e63296389b1e
Andria Bakti Mahendra <andr.bkti@gmail.com>
2021-09-21 19:36:31 +0700

    Add  UPDATE, DELETE and GET menu by ID

--- src/controllers/menu.js
+++ src/controllers/menu.js
@@ -1,15 +1,25 @@
-const {_getAllMenu, _insertMenu} = require('../models/menu')
+const {_getAllMenu, _getMenuById, _insertMenu, _updateMenu, _deleteMenu } = require('../models/menu')
 const { response } = require('../helpers/helpers')
 
 module.exports = {
   getAllMenu: (_req, resp) => {
     _getAllMenu()
-    .then((res) => {
-      response(resp, res, 200, null)
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+      .then((res) => {
+        response(resp, res, 200, null)
+      })
+      .catch((err) => {
+        console.log(err.message);
+      })
+  },
+  getMenuById: (req, resp) => {
+    const {id} = req.params
+    _getMenuById(id)
+      .then((res) => {
+        response(resp, res, 200, null)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
   },
   insertMenu: (req, resp) => {
     const {name, price, description, image} = req.body
@@ -22,11 +32,39 @@ module.exports = {
       updated_at: null
     }
     _insertMenu(data)
-    .then((res) => {
-      response(resp, res, 200, null)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+      .then((res) => {
+        response(resp, res, 200, null)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  },
+  updateMenu: (req, resp) => {
+    const { id } = req.params
+    const { name, price, description, image } = req.body
+    const data = {
+      name,
+      price,
+      description,
+      image,
+      updated_at: new Date()
+    }
+    _updateMenu(data, id)
+      .then((res) => {
+        response(resp, res, 200, null)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  },
+  deleteMenu: (req, resp) => {
+    const {id} = req.params
+    _deleteMenu(id)
+      .then((res) => {
+        response(resp, res, 200, null)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
   }
 }
--- src/models/menu.js
+++ src/models/menu.js
@@ -4,11 +4,24 @@ module.exports = {
   _getAllMenu: () => {
     return actionQuery('SELECT * FROM menu')
   },
-  _insertMenu: ({name, price, description, image, created_at, updated_at}) => {
+  _getMenuById: (id) => {
+    return actionQuery('SELECT * FROM menu WHERE menu_id = $1', [id])
+  },
+  _insertMenu: ({name, price, description, image, created_at}) => {
     const queryData = {
-      text: 'INSERT INTO menu(name, price, description, image, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6)',
-      values: [name, price, description, image, created_at, updated_at]
+      text: 'INSERT INTO menu(name, price, description, image, created_at) VALUES($1, $2, $3, $4, $5)',
+      values: [name, price, description, image, created_at]
     }
     return actionQuery(queryData)
+  },
+  _updateMenu: ({name, price, description, image, updated_at}, id) => {
+    const queryData = {
+      text: 'UPDATE menu SET name = $1, price = $2, description = $3, image = $4, updated_at = $5 WHERE menu_id = $6',
+      values: [name, price, description, image, updated_at, id]
+    }
+    return actionQuery(queryData)
+  },
+  _deleteMenu: (id) => {
+    return actionQuery('DELETE FROM menu WHERE menu_id = $1', [id])
   }
 }
