2c60387ec1517f255b10e68e0927f2c4915f01b4
Andria Bakti Mahendra <andr.bkti@gmail.com>
2021-10-25 21:39:35 +0700

    Add auth routes

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
 const productRoutes = require('./router_product')
 const categoryRoutes = require('./router_category')
-const userRouter = require('./router_user')
+const userRoutes = require('./router_user')
+const authRoutes = require('./router_auth')
 const router = Router()
 
 module.exports = router
-	.use('/products', productRoutes)
+	.use('/auth', authRoutes)
+	.use('/user', userRoutes)
+	.use('/product', productRoutes)
 	.use('/category', categoryRoutes)
-	.use('/user', userRouter)
